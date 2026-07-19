f43073660b002d30a0ee498ba961c27d1a49f489
modalaiso <mobioisrael@hotmail.com>
2026-07-19 13:10:34 +0000

    Remove this unused import of 'useRef'.

--- src/components/posts/comment-section.tsx
+++ src/components/posts/comment-section.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState, useTransition } from "react";
+import { useEffect, useMemo, useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
 import {
   MoreVertical,
